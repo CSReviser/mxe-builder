93ef91b05aab9877041889c226f6e66c28840419
CSReviser <46049273+CSReviser@users.noreply.github.com>
2025-04-19 13:32:06 +0900

    remove-mariadb.sh を更新

--- remove-mariadb.sh
+++ remove-mariadb.sh
@@ -13,10 +13,14 @@ for file in $FILES; do
     fi
 done
 
-# 不要ならファイル自体も削除
+# 不要なファイルを削除
 if [ -f ./mxe/src/mariadb-connector-c.mk ]; then
     echo "Removing mariadb-connector-c.mk"
     rm ./mxe/src/mariadb-connector-c.mk
 fi
+
+# キャッシュをクリア
+echo "Clearing MXE build cache..."
+rm -rf mxe/usr/* mxe/log/* mxe/pkg/* mxe/tmp/*
 
 echo "Cleanup complete."
