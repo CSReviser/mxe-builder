275649be7b3097edc082e95f1c628ae7eb7a8815
CSReviser <46049273+CSReviser@users.noreply.github.com>
2025-04-19 13:29:52 +0900

    remove-mariadb.sh を更新

--- remove-mariadb.sh
+++ remove-mariadb.sh
@@ -1,24 +1,22 @@
-#!/bin/bash
-# remove-mariadb.sh
-
+#!/usr/bin/env bash
 set -e
 
 echo "Safely removing mariadb-connector-c from .mk files..."
 
-find . -name '*.mk' -print0 | while IFS= read -r -d '' file; do
+FILES=$(find ./mxe/src -name "*.mk" -type f)
+
+for file in $FILES; do
     if grep -q 'mariadb-connector-c' "$file"; then
         echo "Processing $file"
-
-        # sedで連結行のmariadb-connector-cを安全に除去
-        sed -i.bak -E '/\\$/ {
-            :a
-            N
-            /\\$/ba
-        } ; s/\\?[[:space:]]*mariadb-connector-c\\?[[:space:]]*\\?\\n//g' "$file"
-
-        # 念のため空行や多重バックスラッシュの整理も
-        sed -i -E '/^$/d' "$file"
+        cp "$file" "$file.bak"
+        sed -i.bak '/mariadb-connector-c/d' "$file"
     fi
 done
+
+# 不要ならファイル自体も削除
+if [ -f ./mxe/src/mariadb-connector-c.mk ]; then
+    echo "Removing mariadb-connector-c.mk"
+    rm ./mxe/src/mariadb-connector-c.mk
+fi
 
 echo "Cleanup complete."
